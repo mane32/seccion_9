757431159ef8b865745ab14333464f8e43ad9673
Vanne Vega <manelp3214@gmail.com>
2022-11-28 18:07:55 -0600

    Get mensajes

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,11 @@
 var express = require('express');
 var router = express.Router();
 
-
+const mensajes = [{
+  _id: '123',
+  user: 'spiderman',
+  mensaje: 'Hola mundo'
+}];
 
 
 
@@ -10,7 +14,8 @@ var router = express.Router();
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  // res.json('Obteniendo mensajes'); 
+  res.json(mensajes);
 });
 
 
